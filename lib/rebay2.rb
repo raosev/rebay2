b67d72ee42798e647d42f515d9df4c3d328eb039
Kirk Jarvis <zuuzlo@yahoo.com>
2016-02-21 21:53:31 -0600

    release 0.2.0

--- lib/rebay2.rb
+++ lib/rebay2.rb
@@ -1,9 +1,8 @@
 require "rebay2/version"
+require 'rebay2/api'
+require 'rebay2/finding'
+require 'rebay2/shopping'
+require 'rebay2/merchandising'
+require 'rebay2/response'
+require 'rebay2/railtie' if defined?(Rails)
 
-module Rebay2
-  require 'rebay2/api'
-  require 'rebay2/finding'
-  require 'rebay2/shopping'
-  require 'rebay2/merchandising'
-  require 'rebay2/response'
-end
